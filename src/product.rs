b4d98f3fd965e3870dd5f9236eed1f59134f65e6
David Peter <mail@david-peter.de>
2022-09-01 23:14:56 +0200

    Canonicalization

--- src/product.rs
+++ src/product.rs
@@ -1,21 +1,23 @@
 use crate::arithmetic::{Exponent, Power};
 
-pub struct Product<Factor> {
+pub struct Product<Factor, const CANONICALIZE: bool = false> {
     factors: Vec<Factor>,
 }
 
-impl<Factor> Product<Factor> {
+impl<Factor: Ord, const CANONICALIZE: bool> Product<Factor, CANONICALIZE> {
     pub fn from_factors(factors: impl IntoIterator<Item = Factor>) -> Self {
-        Self {
-            factors: factors.into_iter().collect(),
-        }
+        Self::from_vec(factors.into_iter().collect())
     }
 
-    pub fn multiply(mut self, mut other: Product<Factor>) -> Product<Factor> {
+    fn from_vec(factors: Vec<Factor>) -> Self {
+        let mut product = Self { factors };
+        product.canonicalize();
+        product
+    }
+
+    pub fn multiply(mut self, mut other: Self) -> Self {
         self.factors.append(&mut other.factors);
-        Product::<Factor> {
-            factors: self.factors,
-        }
+        Self::from_vec(self.factors)
     }
 
     pub fn iter(&self) -> ProductIter<Factor> {
@@ -24,39 +26,38 @@ impl<Factor> Product<Factor> {
         }
     }
 
-    pub fn sort_by_key<K, F>(&mut self, f: F)
-    where
-        F: FnMut(&Factor) -> K,
-        K: Ord,
-    {
-        self.factors.sort_by_key(f);
-    }
-
     #[cfg(test)]
     fn into_vec(self) -> Vec<Factor> {
         self.factors
     }
-}
 
-impl<Factor: Ord> Product<Factor> {
-    pub fn sort(&mut self) {
-        self.factors.sort();
+    pub fn sort_unstable(&mut self) {
+        self.factors.sort_unstable();
+    }
+
+    fn canonicalize(&mut self) {
+        if CANONICALIZE {
+            self.sort_unstable();
+
+        }
     }
 }
 
-impl<Factor: Power> Power for Product<Factor> {
+impl<Factor: Power + Ord, const CANONICALIZE: bool> Power for Product<Factor, CANONICALIZE> {
     fn power(self, exp: Exponent) -> Self {
         Product::from_factors(self.factors.into_iter().map(|f| f.power(exp)))
     }
 }
 
-impl<Factor: Power> Product<Factor> {
+impl<Factor: Power + Ord, const CANONICALIZE: bool> Product<Factor, CANONICALIZE> {
     pub fn invert(self) -> Self {
         self.power(-1)
     }
 
-    pub fn divide(self, other: Product<Factor>) -> Product<Factor> {
-        self.multiply(other.invert())
+    pub fn divide(self, other: Self) -> Self {
+        let mut result = self.multiply(other.invert());
+        result.canonicalize();
+        result
     }
 }
 
@@ -83,8 +84,26 @@ fn test_multiply() {
     );
 }
 
+#[test]
+fn test_multiply_canonicalize() {
+    let product1 = Product::<TestUnit, true>::from_factors([
+        TestUnit("meter".into(), 1),
+        TestUnit("second".into(), 1),
+    ]);
+    let product2 = Product::from_factors([TestUnit("meter".into(), 2)]);
+    let result = product1.multiply(product2);
+    assert_eq!(
+        result.into_vec(),
+        &[
+            TestUnit("meter".into(), 1),
+            TestUnit("meter".into(), 2), // TODO: more aggressive canonicalization (merge consecutive)
+            TestUnit("second".into(), 1)
+        ]
+    );
+}
+
 #[cfg(test)]
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
 struct TestUnit(String, Exponent);
 
 #[cfg(test)]
@@ -96,8 +115,10 @@ impl Power for TestUnit {
 
 #[test]
 fn test_power() {
-    let product =
-        Product::from_factors([TestUnit("meter".into(), 1), TestUnit("second".into(), -2)]);
+    let product = Product::<TestUnit>::from_factors([
+        TestUnit("meter".into(), 1),
+        TestUnit("second".into(), -2),
+    ]);
     let result = product.power(3);
     assert_eq!(
         result.into_vec(),
@@ -107,8 +128,10 @@ fn test_power() {
 
 #[test]
 fn test_divide() {
-    let product1 =
-        Product::from_factors([TestUnit("meter".into(), 1), TestUnit("second".into(), 1)]);
+    let product1 = Product::<TestUnit>::from_factors([
+        TestUnit("meter".into(), 1),
+        TestUnit("second".into(), 1),
+    ]);
     let product2 = Product::from_factors([TestUnit("second".into(), 1)]);
     let result = product1.divide(product2);
     assert_eq!(
@@ -120,6 +143,7 @@ fn test_divide() {
         ]
     );
 }
+
 
 #[test]
 fn test_iter() {
@@ -133,26 +157,11 @@ fn test_iter() {
 }
 
 #[test]
-fn test_sort() {
+fn test_sort_unstable() {
     let mut product = Product::<i32>::from_factors([5, 2, 3]);
-    product.sort();
+    product.sort_unstable();
     assert_eq!(
         product.iter().cloned().collect::<Vec<_>>().as_slice(),
         [2, 3, 5]
-    );
-}
-
-#[test]
-fn test_sort_by_key() {
-    let mut product = Product::<_>::from_factors([(1.0, 2), (2.0, 1), (3.0, -3)]);
-    product.sort_by_key(|(_, second)| *second);
-    assert_eq!(
-        product
-            .iter()
-            .map(|(first, _)| first)
-            .cloned()
-            .collect::<Vec<_>>()
-            .as_slice(),
-        [3.0, 2.0, 1.0]
     );
 }
