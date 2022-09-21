62c228bf3f71b30f45bfbd941c5c3e1f17d2e2b0
David Peter <mail@david-peter.de>
2022-09-21 22:57:59 +0200

    Make sure that examples properly fail with errors

--- tests/prelude_and_examples.rs
+++ tests/prelude_and_examples.rs
@@ -5,41 +5,64 @@ use insect::typechecker::typecheck;
 
 use std::ffi::OsStr;
 use std::fs;
-use std::fs::ReadDir;
 
-fn assert_interpret_result_is_ok(code: &str) {
+fn assert_typechecks_and_runs(code: &str) {
     let statements = parse(&code).unwrap();
     let statements_checked = typecheck(statements).unwrap();
+    assert!(BytecodeInterpreter::new(false)
+        .interpret_statements(&statements_checked)
+        .is_ok());
+}
 
-    let mut interpreter = BytecodeInterpreter::new(false);
+fn assert_typecheck_error(code: &str) {
+    let statements = parse(&code).unwrap();
+    assert!(typecheck(statements).is_err());
+}
 
-    let result = interpreter.interpret_statements(&statements_checked);
-
-    assert!(result.is_ok());
+fn assert_interpreter_error(code: &str) {
+    let statements = parse(&code).unwrap();
+    let statements_checked = typecheck(statements).unwrap();
+    assert!(BytecodeInterpreter::new(false)
+        .interpret_statements(&statements_checked)
+        .is_err());
 }
 
 #[test]
 fn prelude_can_be_parsed_and_interpreted() {
     let prelude_code = fs::read_to_string("prelude.ins").unwrap();
-    assert_interpret_result_is_ok(&prelude_code);
+    assert_typechecks_and_runs(&prelude_code);
 }
 
-#[test]
-fn examples_can_be_parsed_and_interpreted() {
+fn run_for_each_insect_file_in(folder: &str, f: impl Fn(&str) -> ()) {
     let prelude_code = fs::read_to_string("prelude.ins").unwrap();
-    for example in fs::read_dir("examples/").unwrap() {
-        let example = example.unwrap();
-
-        if example.path().extension() != Some(OsStr::new("ins")) {
+    for entry in fs::read_dir(folder).unwrap() {
+        let path = entry.unwrap().path();
+        if path.extension() != Some(OsStr::new("ins")) {
             continue;
         }
-        println!("Testing example {:?}", example=example.path());
-        let example_code = fs::read_to_string(example.path()).unwrap();
-        assert_interpret_result_is_ok(&format!(
+
+        println!("Testing example {:?}", example = path);
+        let example_code = fs::read_to_string(path).unwrap();
+        f(&format!(
             "{prelude}\n\
              {example}",
             prelude = prelude_code,
             example = example_code
         ));
     }
+}
+
+#[test]
+fn examples_can_be_parsed_and_interpreted() {
+    run_for_each_insect_file_in("examples/", assert_typechecks_and_runs);
+}
+
+#[test]
+fn typecheck_error_examples_fail_as_expected() {
+    run_for_each_insect_file_in("examples/typecheck_error", assert_typecheck_error);
+}
+
+#[test]
+fn interpreter_error_examples_fail_as_expected() {
+    run_for_each_insect_file_in("examples/interpreter_error", assert_interpreter_error);
 }
