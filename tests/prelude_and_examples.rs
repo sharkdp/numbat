c597941d77d306234390616cca4e37c2cc14b09c
David Peter <mail@david-peter.de>
2022-09-21 21:39:14 +0200

    Test interpretability of all examples

--- tests/prelude_and_examples.rs
+++ tests/prelude_and_examples.rs
@@ -3,12 +3,12 @@ use insect::interpreter::Interpreter;
 use insect::parser::parse;
 use insect::typechecker::typecheck;
 
+use std::ffi::OsStr;
 use std::fs;
+use std::fs::ReadDir;
 
-#[test]
-fn prelude_can_be_parsed_and_interpreted() {
-    let prelude_code = fs::read_to_string("prelude.ins").unwrap();
-    let statements = parse(&prelude_code).unwrap();
+fn assert_interpret_result_is_ok(code: &str) {
+    let statements = parse(&code).unwrap();
     let statements_checked = typecheck(statements).unwrap();
 
     let mut interpreter = BytecodeInterpreter::new(false);
@@ -19,6 +19,27 @@ fn prelude_can_be_parsed_and_interpreted() {
 }
 
 #[test]
+fn prelude_can_be_parsed_and_interpreted() {
+    let prelude_code = fs::read_to_string("prelude.ins").unwrap();
+    assert_interpret_result_is_ok(&prelude_code);
+}
+
+#[test]
 fn examples_can_be_parsed_and_interpreted() {
-    // TODO
+    let prelude_code = fs::read_to_string("prelude.ins").unwrap();
+    for example in fs::read_dir("examples/").unwrap() {
+        let example = example.unwrap();
+
+        if example.path().extension() != Some(OsStr::new("ins")) {
+            continue;
+        }
+        println!("Testing example {:?}", example=example.path());
+        let example_code = fs::read_to_string(example.path()).unwrap();
+        assert_interpret_result_is_ok(&format!(
+            "{prelude}\n\
+             {example}",
+            prelude = prelude_code,
+            example = example_code
+        ));
+    }
 }
