0690143a1b1c2392270621a38c59618c25154f47
David Peter <mail@david-peter.de>
2022-10-03 22:56:44 +0200

    Structure project into multiple crates

--- insect/tests/prelude_and_examples.rs
+++ insect/tests/prelude_and_examples.rs
@@ -29,12 +29,12 @@ fn assert_interpreter_error(code: &str) {
 
 #[test]
 fn prelude_can_be_parsed_and_interpreted() {
-    let prelude_code = fs::read_to_string("prelude.ins").unwrap();
+    let prelude_code = fs::read_to_string("../prelude.ins").unwrap();
     assert_typechecks_and_runs(&prelude_code);
 }
 
 fn run_for_each_insect_file_in(folder: &str, f: impl Fn(&str)) {
-    let prelude_code = fs::read_to_string("prelude.ins").unwrap();
+    let prelude_code = fs::read_to_string("../prelude.ins").unwrap();
     for entry in fs::read_dir(folder).unwrap() {
         let path = entry.unwrap().path();
         if path.extension() != Some(OsStr::new("ins")) {
@@ -54,15 +54,15 @@ fn run_for_each_insect_file_in(folder: &str, f: impl Fn(&str)) {
 
 #[test]
 fn examples_can_be_parsed_and_interpreted() {
-    run_for_each_insect_file_in("examples/", assert_typechecks_and_runs);
+    run_for_each_insect_file_in("../examples/", assert_typechecks_and_runs);
 }
 
 #[test]
 fn typecheck_error_examples_fail_as_expected() {
-    run_for_each_insect_file_in("examples/typecheck_error", assert_typecheck_error);
+    run_for_each_insect_file_in("../examples/typecheck_error", assert_typecheck_error);
 }
 
 #[test]
 fn interpreter_error_examples_fail_as_expected() {
-    run_for_each_insect_file_in("examples/interpreter_error", assert_interpreter_error);
+    run_for_each_insect_file_in("../examples/interpreter_error", assert_interpreter_error);
 }
