c9fe9b44c34b4109a25d795ed266b1819c8fe7d4
David Peter <mail@david-peter.de>
2023-02-13 20:37:29 +0100

    More prefixes

--- insect/src/prefix.rs
+++ insect/src/prefix.rs
@@ -49,12 +49,19 @@ impl Prefix {
 impl Display for Prefix {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
+            Prefix::Decimal(-18) => write!(f, "atto"),
+            Prefix::Decimal(-15) => write!(f, "femto"),
+            Prefix::Decimal(-12) => write!(f, "pico"),
+            Prefix::Decimal(-9) => write!(f, "nano"),
+            Prefix::Decimal(-6) => write!(f, "micro"),
             Prefix::Decimal(-3) => write!(f, "milli"),
             Prefix::Decimal(-2) => write!(f, "centi"),
             Prefix::Decimal(-1) => write!(f, "deci"),
             Prefix::Decimal(0) => write!(f, ""),
             Prefix::Decimal(3) => write!(f, "kilo"),
             Prefix::Decimal(6) => write!(f, "mega"),
+            Prefix::Decimal(9) => write!(f, "giga"),
+            Prefix::Decimal(12) => write!(f, "tera"),
             _ => write!(f, "<prefix>"), // TODO
         }
     }
