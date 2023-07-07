12e202c0e9982a77e808aeb67131dca7da0ec5ed
David Peter <mail@david-peter.de>
2023-07-07 15:07:41 +0200

    Fix wasm tests

--- numbat-wasm/tests/web.rs
+++ numbat-wasm/tests/web.rs
@@ -1,11 +1,11 @@
 #![cfg(target_arch = "wasm32")]
 
-use wasm_bindgen_test::*;
 use numbat_wasm::interpret;
+use wasm_bindgen_test::*;
 
 wasm_bindgen_test_configure!(run_in_browser);
 
 #[wasm_bindgen_test]
 fn basic() {
-    assert_eq!(interpret("2 + 3 * 4"), "14.000000 ");
+    assert_eq!(interpret("2 + 3 * 4"), "14 ");
 }
