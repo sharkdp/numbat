eefafa8e242eb92196b878b13b98aec75e9f78d3
David Peter <mail@david-peter.de>
2023-09-20 09:38:09 +0200

    Add pretty-printing

--- numbat-wasm/src/jquery_terminal_formatter.rs
+++ numbat-wasm/src/jquery_terminal_formatter.rs
@@ -2,11 +2,19 @@ use numbat::markup::{FormatType, FormattedString, Formatter};
 
 pub struct JqueryTerminalFormatter;
 
-pub fn jt_format(class: &str, content: &str) -> String {
+pub fn jt_format(class: Option<&str>, content: &str) -> String {
     if content.is_empty() {
-        "".into()
-    } else {
+        return "".into();
+    }
+
+    let content = html_escape::encode_text(content)
+        .replace("[", "&#91;")
+        .replace("]", "&#93;");
+
+    if let Some(class) = class {
         format!("[[;;;hl-{class}]{content}]")
+    } else {
+        content.into()
     }
 }
 
@@ -16,18 +24,18 @@ impl Formatter for JqueryTerminalFormatter {
         FormattedString(_output_type, format_type, s): &FormattedString,
     ) -> String {
         let css_class = match format_type {
-            FormatType::Whitespace => return s.clone(),
-            FormatType::Dimmed => "dimmed",
-            FormatType::Text => return s.clone(),
-            FormatType::String => "string",
-            FormatType::Keyword => "keyword",
-            FormatType::Value => "value",
-            FormatType::Unit => "unit",
-            FormatType::Identifier => "identifier",
-            FormatType::TypeIdentifier => "type-identifier",
-            FormatType::Operator => "operator",
-            FormatType::Decorator => "decorator",
+            FormatType::Whitespace => None,
+            FormatType::Dimmed => Some("dimmed"),
+            FormatType::Text => None,
+            FormatType::String => Some("string"),
+            FormatType::Keyword => Some("keyword"),
+            FormatType::Value => Some("value"),
+            FormatType::Unit => Some("unit"),
+            FormatType::Identifier => Some("identifier"),
+            FormatType::TypeIdentifier => Some("type-identifier"),
+            FormatType::Operator => Some("operator"),
+            FormatType::Decorator => Some("decorator"),
         };
-        jt_format(&css_class, s)
+        jt_format(css_class, s)
     }
 }
