8779de9d0ebb6282111ce50a8bca04ff2fdf16ad
David Peter <mail@david-peter.de>
2023-09-28 22:35:50 +0200

    Change deploy path

--- numbat-wasm/deploy.sh
+++ numbat-wasm/deploy.sh
@@ -10,5 +10,5 @@ wasm-pack build --features=wee_alloc
     npm run build
 )
 
-rsync --archive --stats --progress --human-readable -r www/ shark.fish:numbat.dev/
-rsync --archive --stats --progress --human-readable -r www/dist/ shark.fish:numbat.dev/
+rsync --archive --stats --progress --human-readable -r www/ shark.fish:numbat.dev/angles/
+rsync --archive --stats --progress --human-readable -r www/dist/ shark.fish:numbat.dev/angles/
