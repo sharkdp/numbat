768d71f1a71e6d2b337542ca327fda8e06a184de
David Peter <mail@david-peter.de>
2023-10-11 09:11:57 +0200

    Add branch check

--- numbat-wasm/deploy.sh
+++ numbat-wasm/deploy.sh
@@ -2,6 +2,13 @@
 
 set -euo pipefail
 
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+
+if [[ "$current_branch" != "master" ]]; then
+    echo "You are currently on the '$current_branch' branch, not 'master'."
+    exit 1
+fi
+
 wasm-pack build --features=wee_alloc
 
 (
