8d13c855cd3abc3a653f596110d26c03357adb81
David Peter <mail@david-peter.de>
2023-11-12 10:14:20 +0100

    Updates

--- numbat-wasm/build-pkg.sh
+++ numbat-wasm/build-pkg.sh
@@ -1,1 +1,10 @@
-wasm-pack build --features=wee_alloc -t web && cp -r pkg/ ../assets/articles
+#!/bin/bash
+
+set -euo pipefail
+
+wasm-pack build --features=wee_alloc -t web
+
+curl https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6/ace.min.js -o pkg/ace.min.js
+
+cp -r pkg/ ../assets/articles
+
