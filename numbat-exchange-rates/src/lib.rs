84ae76626a07682b47342b96b119061e814f1d61
David Peter <mail@david-peter.de>
2023-12-19 22:58:15 +0100

    Make attohttpc dependency optional, shrink WASM size from 1.3 MiB to 800 KiB

--- numbat-exchange-rates/src/lib.rs
+++ numbat-exchange-rates/src/lib.rs
@@ -33,18 +33,22 @@ pub fn parse_exchange_rates(xml_content: &str) -> Option<ExchangeRates> {
     Some(rates)
 }
 
+#[cfg(feature = "fetch-exchangerates")]
 const ECB_XML_URL: &str = "https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml";
 
+#[cfg(feature = "fetch-exchangerates")]
 fn fetch_ecb_xml() -> Option<String> {
     attohttpc::get(ECB_XML_URL).send().ok()?.text().ok()
 }
 
+#[cfg(feature = "fetch-exchangerates")]
 pub fn fetch_exchange_rates() -> Option<ExchangeRates> {
     let xml_content = fetch_ecb_xml()?;
     parse_exchange_rates(&xml_content)
 }
 
 #[cfg(test)]
+#[cfg(feature = "fetch-exchangerates")]
 mod tests {
     use super::*;
 
