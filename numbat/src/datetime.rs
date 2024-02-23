79d92c69e69cdd0c960fea3a26625d3299d52ec5
David Peter <mail@david-peter.de>
2024-02-24 00:39:03 +0100

    Always return specified offset/tz from datetime(…)

--- numbat/src/datetime.rs
+++ numbat/src/datetime.rs
@@ -1,4 +1,4 @@
-use chrono::{DateTime, Datelike, FixedOffset, LocalResult, Offset, TimeZone};
+use chrono::{DateTime, Datelike, FixedOffset, LocalResult};
 use chrono_tz::Tz;
 
 pub fn get_local_timezone() -> Option<Tz> {
@@ -8,21 +8,6 @@ pub fn get_local_timezone() -> Option<Tz> {
 
 pub fn get_local_timezone_or_utc() -> Tz {
     get_local_timezone().unwrap_or(chrono_tz::UTC)
-}
-
-/// We use this function to get the UTC offset corresponding to
-/// the local timezone *at the specific instant in time* specified
-/// by 'dt', which might be different from the *current* UTC offset.
-///
-/// For example, in the timezone Europe/Berlin in 2024, we expect a
-/// UTC offset of +01:00 for 'dt's in winter, while we expect a UTC
-/// offset of +02:00 for 'dt's in summer, due to DST. If we were to
-/// use chronos 'Local', we would get a UTC offset depending on when
-/// we run the program.
-pub fn local_offset_for_datetime<O: TimeZone>(dt: &DateTime<O>) -> FixedOffset {
-    get_local_timezone()
-        .map(|tz| dt.with_timezone(&tz).offset().fix())
-        .unwrap_or_else(|| dt.offset().fix())
 }
 
 pub fn parse_datetime(input: &str) -> Option<DateTime<FixedOffset>> {
@@ -62,7 +47,7 @@ pub fn parse_datetime(input: &str) -> Option<DateTime<FixedOffset>> {
                 if let Ok(tz) = potential_timezone_name.parse::<Tz>() {
                     if let Ok(ndt) = chrono::NaiveDateTime::parse_from_str(rest, format) {
                         if let LocalResult::Single(dt) = ndt.and_local_timezone(tz) {
-                            return Some(dt.with_timezone(&local_offset_for_datetime(&dt)));
+                            return Some(dt.fixed_offset());
                         }
                     }
                 }
@@ -70,10 +55,9 @@ pub fn parse_datetime(input: &str) -> Option<DateTime<FixedOffset>> {
 
             // Without timezone/offset
             if let Ok(ndt) = chrono::NaiveDateTime::parse_from_str(input, format) {
-                if let LocalResult::Single(dt) =
-                    ndt.and_local_timezone(get_local_timezone().unwrap_or(chrono_tz::UTC))
+                if let LocalResult::Single(dt) = ndt.and_local_timezone(get_local_timezone_or_utc())
                 {
-                    return Some(dt.with_timezone(&local_offset_for_datetime(&dt)));
+                    return Some(dt.fixed_offset());
                 }
             }
         }
