1707f6d73c53c1bc87a3bf600b3006e4beef0273
Marko Bausch <60338487+mrkbac@users.noreply.github.com>
2024-03-19 18:21:34 +0100

    Switch to rand and remove test

--- numbat/tests/interpreter.rs
+++ numbat/tests/interpreter.rs
@@ -362,7 +362,6 @@ fn test_other_functions() {
     expect_output("floor(3.9)", "3");
     expect_output("round(3.9)", "4");
     expect_output("round(3.1)", "3");
-    expect_output("random() == random()", "false");
 }
 
 #[test]
