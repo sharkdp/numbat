22e63e22eaa6ed46c80a36e701832a3e8c706c8c
David Peter <mail@david-peter.de>
2024-06-04 15:47:12 +0200

    Reset deploy script

--- numbat-wasm/deploy.sh
+++ numbat-wasm/deploy.sh
@@ -4,11 +4,11 @@ set -euo pipefail
 
 current_branch=$(git rev-parse --abbrev-ref HEAD)
 
-if [[ "$current_branch" != "type-inference" ]]; then
+if [[ "$current_branch" != "master" ]]; then
     echo "You are currently on the '$current_branch' branch, not 'master'."
     exit 1
 fi
 
 bash build.sh
 
-rsync --archive --stats --progress --human-readable -r www/ shark.fish:numbat.dev/type-inference/
+rsync --archive --stats --progress --human-readable -r www/ shark.fish:numbat.dev/
