e983af91394c4350d2fc4ccf62bd63ccac06a8bf
Tamo <tamo@meilisearch.com>
2024-07-28 14:25:11 +0200

    use egui instead of plotly

--- numbat/src/plot.rs
+++ numbat/src/plot.rs
@@ -1,32 +1,22 @@
-use plotly::{
-    color::Rgb,
-    common::{Font, Line},
-    layout::Axis,
-    Configuration, Layout, Plot, Scatter,
-};
+use egui_plot::{Line, Plot, PlotPoints};
 
 pub fn line_plot(xs: Vec<f64>, ys: Vec<f64>, x_label: &str, y_label: &str) {
-    let x_min = xs[0];
-    let x_max = xs[xs.len() - 1];
-    let y_min = ys.iter().fold(f64::INFINITY, |a, &b| a.min(b));
-    let y_max = ys.iter().fold(f64::NEG_INFINITY, |a, &b| a.max(b));
+    let options = eframe::NativeOptions::default();
+    let x_label = x_label.to_string();
+    let y_label = y_label.to_string();
 
-    let y_max = y_max + 0.1 * (y_max - y_min);
-    let y_min = y_min - 0.1 * (y_max - y_min);
-
-    let mut plot = Plot::new();
-    let trace = Scatter::new(xs, ys).line(Line::new().color(Rgb::new(0x00, 0x77, 0xff)));
-    plot.add_trace(trace);
-
-    let layout = Layout::new()
-        .x_axis(Axis::new().title(x_label).range(vec![x_min, x_max]))
-        .y_axis(Axis::new().title(y_label).range(vec![y_min, y_max]))
-        .font(Font::new().size(16).family("Lato, Roboto, sans-serif"))
-        .width(1200);
-    plot.set_layout(layout);
-
-    plot.set_configuration(Configuration::new());
-
-    // plot.use_local_plotly();
-    plot.show();
+    eframe::run_simple_native("Plot", options, move |ctx, _frame| {
+        egui::TopBottomPanel::top("Top panel").show(ctx, |ui| {
+            egui::widgets::global_dark_light_mode_buttons(ui);
+        });
+        egui::CentralPanel::default().show(ctx, |ui| {
+            let sin: PlotPoints = xs.iter().zip(&ys).map(|(x, y)| [*x, *y]).collect();
+            let line = Line::new(sin);
+            Plot::new("my_plot")
+                .x_axis_label(&x_label)
+                .y_axis_label(&y_label)
+                .show(ui, |plot_ui| plot_ui.line(line));
+        });
+    })
+    .unwrap();
 }
