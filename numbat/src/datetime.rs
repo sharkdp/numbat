20cd200a2d7e8f31892b71ea82b3fc736075e842
David Peter <mail@david-peter.de>
2024-07-31 21:21:24 +0200

    Improve parse_datetime, add tests

--- numbat/src/datetime.rs
+++ numbat/src/datetime.rs
@@ -1,59 +1,71 @@
-use jiff::{civil::DateTime, fmt::rfc2822, tz::TimeZone, Zoned};
+use jiff::{civil::DateTime, fmt::rfc2822, tz::TimeZone, Timestamp, Zoned};
+use std::str::FromStr;
 
 pub fn get_local_timezone_or_utc() -> TimeZone {
     TimeZone::system()
 }
 
-pub fn parse_datetime(input: &str) -> Option<Zoned> {
-    if let Ok(dt) = DateTime::strptime("%Y-%m-%dT%H:%M:%SZ", input) {
-        // RFC 3339
-        Some(dt.to_zoned(TimeZone::UTC).unwrap()) // TODO
-    } else if let Ok(dt) = rfc2822::parse(input) {
-        Some(dt)
-    } else {
-        const FORMATS: [&str; 8] = [
-            // 24 hour formats:
-            "%Y-%m-%d %H:%M:%S%.f",
-            "%Y/%m/%d %H:%M:%S%.f",
-            "%Y-%m-%d %H:%M",
-            "%Y/%m/%d %H:%M",
-            // 12 hour formats:
-            "%Y-%m-%d %I:%M:%S %p%.f",
-            "%Y-%m-%d %I:%M %p",
-            "%Y/%m/%d %I:%M:%S %p%.f",
-            "%Y/%m/%d %I:%M %p",
-        ];
+pub fn parse_datetime(input: &str) -> Result<Zoned, jiff::Error> {
+    if let zoned @ Ok(_) = Zoned::from_str(input) {
+        return zoned;
+    }
 
-        for format in FORMATS {
-            // Try to match the given format plus an additional UTC offset (%z)
-            if let Ok(dt) = Zoned::strptime(&format!("{format} %z"), input) {
-                return Some(dt);
-            }
+    // RFC 3339
+    if let Ok(timestamp) = DateTime::strptime("%Y-%m-%dT%H:%M:%S%.fZ", input) {
+        if let zoned @ Ok(_) = timestamp.to_zoned(TimeZone::UTC) {
+            return zoned;
+        }
+    }
 
-            // Try to match the given format plus an additional timezone name (%Z).
-            // jiff does not support %Z, so we implement this ourselves. We were
-            // warned by developers before us not to write timezone-related code on
-            // our own, so we're probably going to regret this.
+    // RFC 2822
+    if let dt @ Ok(_) = rfc2822::parse(input) {
+        return dt;
+    }
 
-            // Get the last space-separated word in the input string, and try to parse it
-            // as a timezone specifier, then try to match the rest of the string with the
-            // given format.
-            if let Some((rest, potential_timezone_name)) = input.rsplit_once(' ') {
-                if let Ok(tz) = TimeZone::get(potential_timezone_name) {
-                    if let Ok(datetime) = DateTime::strptime(format, rest) {
-                        return Some(datetime.to_zoned(tz).unwrap()); // TODO
-                    }
+    const FORMATS: [&str; 8] = [
+        // 24 hour formats:
+        "%Y-%m-%d %H:%M:%S%.f",
+        "%Y/%m/%d %H:%M:%S%.f",
+        "%Y-%m-%d %H:%M",
+        "%Y/%m/%d %H:%M",
+        // 12 hour formats:
+        "%Y-%m-%d %I:%M:%S %p%.f",
+        "%Y-%m-%d %I:%M %p",
+        "%Y/%m/%d %I:%M:%S %p%.f",
+        "%Y/%m/%d %I:%M %p",
+    ];
+
+    for format in FORMATS {
+        // Try to match the given format plus an additional UTC offset (%z)
+        if let Ok(dt) = Zoned::strptime(&format!("{format} %z"), input) {
+            return Ok(dt);
+        }
+
+        // Try to match the given format plus an additional timezone name. This is
+        // similar to '%Z', which is not supported for parsing in jiff. The reason
+        // for this is that it can be ambiguous. CST, for example, has several
+        // meanings.
+        // We were warned by developers before us not to write timezone-related
+        // code on our own, so we're probably going to regret this.
+
+        // Get the last space-separated word in the input string, and try to parse it
+        // as a timezone specifier, then try to match the rest of the string with the
+        // given format.
+        if let Some((rest, potential_timezone_name)) = input.rsplit_once(' ') {
+            if let Ok(tz) = TimeZone::get(potential_timezone_name) {
+                if let Ok(datetime) = DateTime::strptime(format, rest) {
+                    return datetime.to_zoned(tz);
                 }
-            }
-
-            // Without timezone/offset
-            if let Ok(dt) = DateTime::strptime(format, input) {
-                return Some(dt.to_zoned(get_local_timezone_or_utc()).unwrap()); // TODO
             }
         }
 
-        None
+        // Without timezone/offset
+        if let Ok(dt) = DateTime::strptime(format, input) {
+            return dt.to_zoned(get_local_timezone_or_utc());
+        }
     }
+
+    Timestamp::from_str(input).map(|ts| ts.to_zoned(get_local_timezone_or_utc()))
 }
 
 pub fn to_string(dt: &Zoned) -> String {
