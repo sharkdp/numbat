93340bb93baf748b0e347650532fba02a919eb40
David Peter <mail@david-peter.de>
2024-08-29 21:03:45 +0200

    Move rustc version check from deploy.sh to build.sh

--- numbat-wasm/deploy.sh
+++ numbat-wasm/deploy.sh
@@ -9,15 +9,6 @@ if [[ "$current_branch" != "master" ]]; then
     exit 1
 fi
 
-# Make sure that Rust version is == 1.76.0 for now to avoid running into
-# https://github.com/rustwasm/wasm-pack/issues/1389
-# With newer versions, we get panics when (for example) running:
-# https://numbat.dev/doc/example-paper_size.html
-if ! rustc --version | grep -q "1.76.0"; then
-    echo "Please switch to Rust version 1.76.0."
-    exit 1
-fi
-
 bash build.sh
 
 rsync --archive --stats --progress --human-readable -r www/ shark.fish:numbat.dev/
