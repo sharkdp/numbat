7c8dc420a74b879ce87d9735f5a0408a945a085f
David Peter <mail@david-peter.de>
2024-08-29 21:24:14 +0200

    Move back to deploy.sh (CI)

--- numbat-wasm/build.sh
+++ numbat-wasm/build.sh
@@ -2,15 +2,5 @@
 
 set -euo pipefail
 
-# Make sure that Rust version is == 1.76.0 for now to avoid running into
-# https://github.com/rustwasm/wasm-pack/issues/1389
-# With newer versions, we get panics when (for example) running:
-# https://numbat.dev/doc/example-paper_size.html
-if ! rustc --version | grep -q "1.76.0"; then
-    echo "Please switch to Rust version 1.76.0."
-    echo "(rustup default 1.76.0)"
-    exit 1
-fi
-
 rm -rf www/pkg
 wasm-pack build --target=web --out-dir=www/pkg
