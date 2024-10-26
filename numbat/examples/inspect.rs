0d47f95d0be38ce6bf9bac7d8724692c11ffced6
Bzero <B_0@posteo.net>
2024-10-26 19:19:14 +0000

    Fail documentation generation for invalid examples

--- numbat/examples/inspect.rs
+++ numbat/examples/inspect.rs
@@ -5,6 +5,7 @@ use numbat::module_importer::FileSystemImporter;
 use numbat::resolver::CodeSource;
 use numbat::Context;
 use std::path::Path;
+use std::process::exit;
 
 const AUTO_GENERATED_HINT: &str = "<!-- NOTE! This file is auto-generated -->";
 
@@ -148,8 +149,9 @@ fn inspect_functions_in_module(ctx: &Context, prelude_ctx: &Context, module: Str
                     println!();
                 } else {
                     eprintln!(
-                        "Warning: Example \"{example_code}\" of function {fn_name} did not run successfully."
+                        "Error: Example \"{example_code}\" of function {fn_name} did not run successfully."
                     );
+                    exit(1);
                 }
             }
             println!("</details>");
