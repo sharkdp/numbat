a549c44773bec4020470d41b582c7b9c1c3519b6
Mads Møller Jensen <mads1606@gmail.com>
2025-04-06 22:42:18 +0200

    Change curly escape to align with python f-strings

--- numbat/src/tokenizer.rs
+++ numbat/src/tokenizer.rs
@@ -363,8 +363,16 @@ impl Tokenizer {
                     break;
                 }
                 Some('\\') if !escaped => true,
-                Some('"') | Some('{') if !escaped => {
+                Some('"') if !escaped => {
                     break;
+                }
+                c @ (Some('{') | Some('}')) if c != self.peek2(input) => {
+                    break;
+                }
+                Some('{') | Some('}') => {
+                    // extra advance to skip both curly's making up the escape sequence
+                    self.advance(input);
+                    false
                 }
                 Some(_) => false,
             };
@@ -1138,9 +1146,9 @@ fn test_tokenize_string() {
     );
 
     insta::assert_snapshot!(
-        tokenize_reduced_pretty(r#""start \{inner\} end""#).unwrap(),
+        tokenize_reduced_pretty(r#""start {{inner}} end""#).unwrap(),
         @r###"
-    "\"start \\{inner\\} end\"", StringFixed, 0
+    "\"start {{inner}} end\"", StringFixed, 0
     "", Eof, 21
     "###
     );
