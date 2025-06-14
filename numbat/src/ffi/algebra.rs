fd8df405434c1f5ab63ad86be0e4aee9eef6b0fe
Tom Webber <t.c.w.webber@gmail.com>
2025-06-14 23:48:38 +0200

    tests: add tests for cubic_equation

--- numbat/src/ffi/algebra.rs
+++ numbat/src/ffi/algebra.rs
@@ -4,6 +4,7 @@ use super::Result;
 use crate::list::NumbatList;
 use crate::quantity::Quantity;
 use crate::value::Value;
+use crate::RuntimeError;
 use std::{cmp::Ordering, f64::consts::PI};
 
 fn _reduced_cubic_equation(a: f64, b: f64) -> Vec<f64> {
@@ -33,27 +34,55 @@ fn _reduced_cubic_equation(a: f64, b: f64) -> Vec<f64> {
     }
 }
 
-fn _solve_cubic_equation(a: f64, b: f64, c: f64, d: f64) -> Vec<f64> {
+fn _solve_square_equation(a: f64, b: f64, c: f64) -> Result<Vec<f64>> {
     if a == 0. {
-        return vec![3.1415];
+        if b == 0. {
+            if c == 0. {
+                Err(Box::new(RuntimeError::UserError(
+                    "infinitely many solutions".to_owned(),
+                )))
+            } else {
+                Ok(vec![])
+            }
+        } else {
+            Ok(vec![-c / b])
+        }
+    } else {
+        let delta = b.powi(2) - 4. * a * c;
+        if delta == 0. {
+            Ok(vec![-b / (2. * a)])
+        } else {
+            let sqrt_d = delta.sqrt();
+            Ok(vec![(-b - sqrt_d) / 2., (-b + sqrt_d) / 2.])
+        }
+    }
+}
+
+fn _solve_cubic_equation(a: f64, b: f64, c: f64, d: f64) -> Result<Vec<f64>> {
+    if a == 0. {
+        return _solve_square_equation(b, c, d);
     }
     let p = b / a;
     let q = c / a;
     let r = d / a;
-    _reduced_cubic_equation(q - p.powi(2) / 3., 2. * p.powi(3) / 27. - p * q / 3. + r)
-        .into_iter()
-        .map(|y| ((y - p / 3.) * 1e14).round() * 1e-14)
-        .collect()
+    Ok(
+        _reduced_cubic_equation(q - p.powi(2) / 3., 2. * p.powi(3) / 27. - p * q / 3. + r)
+            .into_iter()
+            .map(|y| ((y - p / 3.) * 1e14).round() * 1e-14)
+            .collect(),
+    )
 }
 
 pub fn cubic_equation(mut args: Args) -> Result<Value> {
+    let mut float_solutions = _solve_cubic_equation(
+        scalar_arg!(args).to_f64(),
+        scalar_arg!(args).to_f64(),
+        scalar_arg!(args).to_f64(),
+        scalar_arg!(args).to_f64(),
+    )?;
+    float_solutions.sort_by(|a, b| a.partial_cmp(b).unwrap());
     let mut solutions: NumbatList<Value> = NumbatList::new();
-    for sol in _solve_cubic_equation(
-        scalar_arg!(args).to_f64(),
-        scalar_arg!(args).to_f64(),
-        scalar_arg!(args).to_f64(),
-        scalar_arg!(args).to_f64(),
-    ) {
+    for sol in float_solutions {
         let sol_val: Value = Value::Quantity(Quantity::from_scalar(sol));
         solutions.push_front(sol_val);
     }
