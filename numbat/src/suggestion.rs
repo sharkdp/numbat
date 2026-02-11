14041631c01f6d9304a00bdf4ad6fd732d2e30bb
David Peter <mail@david-peter.de>
2026-02-11 19:52:35 +0100

    Snapshot tests for error messages

--- numbat/src/suggestion.rs
+++ numbat/src/suggestion.rs
@@ -16,7 +16,7 @@ pub fn did_you_mean<S: AsRef<str>, T: AsRef<str>>(
                 ),
             )
         })
-        .min_by_key(|(_, dist)| *dist)
+        .min_by(|(id_a, dist_a), (id_b, dist_b)| dist_a.cmp(dist_b).then(id_a.cmp(id_b)))
         .filter(|(id, dist)| id.len() >= 2 && *dist <= 3)
         .map(|(id, _)| id)
 }
