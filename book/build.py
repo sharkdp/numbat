e61cff4ac15a879dd672498fa8a9290dfb7cdfdf
Ryan Gast <ryan.d.gast@gmail.com>
2026-03-13 16:25:28 -0500

    Add matrix indexing and multiplication example

--- book/build.py
+++ book/build.py
@@ -74,6 +74,7 @@ def generate_all_examples():
     generate_example("body_mass_index", "Body mass index")
     generate_example("factorial", "Factorial", strip_asserts=False)
     generate_example("medication_dosage", "Medication dosage")
+    generate_example("matrix", "Matrices")
     generate_example("molarity", "Molarity")
     generate_example("musical_note_frequency", "Musical note frequency")
     generate_example("paper_size", "Paper sizes")
